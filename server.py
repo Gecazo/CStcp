b2221a6c0e498e7b9884c3c58e66b630485d5741
Filip <43730405+Gecazo@users.noreply.github.com>
2020-09-02 00:24:39 +0300

    Server update

--- server.py
+++ server.py
@@ -1,17 +1,43 @@
-import socket
+import socket 
+import threading
 
-sock: socket = socket.socket()
-print('Socket created')
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
-sock.bind(('localhost', 1000))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
-sock.listen(3)
-print('Waiting for connections')
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
 
-while True:
-    client_socket, address = sock.accept()
-    user_name = client_socket.recv(1024).decode()
-    print('Connected with ', address, user_name)
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
 
-    client_socket.send(bytes("Connected",'utf-8'))
-    client_socket.close()
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+
+    conn.close()
+        
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
