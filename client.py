678502a133c05ee3529f1429eabb3f44c1cd965a
Filip <43730405+Gecazo@users.noreply.github.com>
2020-11-23 16:05:05 +0100

    Client update
    Exit has been added

--- client.py
+++ client.py
@@ -1,28 +1,28 @@
 import socket
 
 HEADER = 64
+SERVER = "192.168.56.1"
 PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.1.26"
 ADDR = (SERVER, PORT)
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 def send(msg):
-    message = msg.encode(FORMAT)
+    message = msg.encode()
     msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
+    send_length = str(msg_length).encode()
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+    print(client.recv(2048).decode())
 
-send("Hello World!")
-input()
-send("Hello Everyone!")
-input()
-send("Hello Tim!")
+print("Type 'q' if you want to exit.")
+while True:
+    message = input("Please enter your message: ")
+    send(message)
+    if message == 'q' or 'quit':
+        break
 
 send(DISCONNECT_MESSAGE)
